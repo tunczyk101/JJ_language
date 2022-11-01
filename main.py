63287a57126712c66e06bc1044d99ee32a1d3a60
DESKTOP-FMC7F7R\jolka <jolka2102@gmail.com>
2022-11-01 20:49:29 +0100

    correct imports main

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import sys
+# import antlr4
 from antlr4 import *
-from parser.out.jjLexer import jjLexer
-from parser.out.jjParser import jjParser
+from parser.out.jj import jj
+from parser.out.jj_lexer import jj_lexer
 from parser.out.jjListener import jjListener
 
 if __name__ == "__main__":
+    print("sss")
     while 1:
-        data =  InputStream(input(">>> "))
-        lexer = jjLexer(data)
+        data = InputStream(input(">>> "))
+        lexer = jj_lexer(data)
         stream = CommonTokenStream(lexer)
-        parser = jjParser(stream)
+        parser = jj(stream)
         tree = parser.expresion()
+        listener = jjListener()
+    #     listener.run(tree)~~
