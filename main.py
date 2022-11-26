aca5d296b7bfee087a85df01a2ae4ab602c811b6
Mapet13 <jakubsordyl1@gmail.com>
2022-11-26 18:27:46 +0100

    add variable stack

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ def check_initial_errors(program_path, verbose):
     listener = jjParserListener(verbose)
     listener.run(tree)
 
-def interprete(program_path, verbose):
+def interprete(program_path):
     data = FileStream(program_path)
     lexer = jjLexer(data)
     stream = CommonTokenStream(lexer)
     parser = jjParser(stream)
     tree = parser.prog()
     
-    visitor = jjVisitor(verbose)
+    visitor = jjVisitor()
     visitor.visit(tree)
 
 
@@ -30,6 +30,6 @@ if __name__ == "__main__":
     #program_path = "examples/only_main.jj"
     program_path = sys.argv[1]
     check_initial_errors(program_path, verbose = False)
-    interprete(program_path, verbose = True)
+    interprete(program_path)
 
     #main("examples/functions.jj")
--- parser/out/jjVisitor.py
+++ parser/out/jjVisitor.py
@@ -1,4 +1,8 @@
+import collections
 from antlr4 import *
+
+from typing import Any
+from dataclasses import dataclass
 
 if __name__ is not None and "." in __name__:
     from .jjParser import jjParser
@@ -10,14 +14,42 @@ else:
     from jjParserVisitor import jjParserVisitor
     from logger import Logger
 
+@dataclass
+class Variable:
+    isMutable: bool
+    name: str
+    value: Any 
 
 class jjVisitor(jjParserVisitor):
 
-
-    def __init__(self, verbose = False):
+    def __init__(self):
         super().__init__()
-        self.logger = Logger(verbose)
+        self.variablesStack = collections.deque()
 
+    def getVariableValue(self, name):
+        for var in self.variablesStack:
+            if var is not None and var.name == name:
+                return var.value
+
+        print(f"ERROR: variable '{name}' is not defined.")
+        exit(0)
+
+    def visitStructural_block(self, ctx: jjParser.Structural_blockContext):
+        self.variablesStack.append(None)
+
+        super().visitStructural_block(ctx)
+        
+        while(self.variablesStack[-1] is not None):
+            self.variablesStack.pop()
+        self.variablesStack.pop()
+
+    def visitVariable_declaration(self, ctx: jjParser.Variable_declarationContext):
+        self.variablesStack.append(Variable(
+            isMutable = ctx.MUTABLE_TOKEN() is not None, 
+            name = str(ctx.NAME()),
+            value = self.visitExpresion(ctx.expresion())
+        ))
+        
     def visitValue(self, ctx: jjParser.ValueContext):
         bool = ctx.BOOL()
         if bool is not None:
@@ -51,23 +83,29 @@ class jjVisitor(jjParserVisitor):
             case '-': return BinaryOperations.sub
 
     def visitExpresion_in_parenthesis(self, ctx: jjParser.Expresion_in_parenthesisContext):
-        return self.visitExpresion(ctx.getChild(0, jjParser.ExpresionContext))
+        return self.visitExpresion(ctx.expresion())
 
     def visitExpresion(self, ctx: jjParser.ExpresionContext):
         if ctx.getChildCount() == 3:
-            left = self.visitLeft_of_binary_operation(ctx.getChild(0, jjParser.Left_of_binary_operationContext))
-            bin_op_fn = self.visitAll_binary_operations(ctx.getChild(0, jjParser.All_binary_operationsContext))
-            right = self.visitExpresion(ctx.getChild(0, jjParser.ExpresionContext))
+            left = self.visitLeft_of_binary_operation(ctx.left_of_binary_operation())
+            bin_op_fn = self.visitAll_binary_operations(ctx.all_binary_operations())
+            right = self.visitExpresion(ctx.expresion())
             return bin_op_fn(left, right)
         
-        unary = ctx.getChild(0, jjParser.All_unary_operationsContext)
+        unary = ctx.all_unary_operations()
         if unary is not None:
-            return self.visitAll_unary_operations(unary)(self.visitExpresion(ctx.getChild(0, jjParser.ExpresionContext)))
+            return self.visitAll_unary_operations(unary)(self.visitExpresion(ctx.expresion()))
 
         return super().visitExpresion(ctx)
 
     def visitChildren(self, node):
         value = super().visitChildren(node)
-        self.logger.log("visited: " + node.getText() + " -> " + str(value))
         return value
+
+    def visitIdentifier(self, ctx: jjParser.IdentifierContext):
+        name = ctx.NAME()
+        if(name is not None):
+            return self.getVariableValue(str(name))
+
+        return self.visitValue(ctx.value())
     
