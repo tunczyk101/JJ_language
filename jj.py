c771ce0d371908555d9e0504f10fd09ff828ecc1
tunczyk101 <jolka2102@gmail.com>
2023-01-07 20:38:24 +0100

    syntax error

--- jj.py
+++ jj.py
@@ -1,14 +1,23 @@
 from antlr4 import *
+
+from parser.out.jjErrorListener import jjErrorListener
 from parser.out.jjParser import jjParser
 from parser.out.jjLexer import jjLexer
 from parser.out.jjListener import jjListener
 from parser.out.jjVisitor import jjVisitor
+
 
 def check_initial_errors(program_path: str, verbose: bool):
     data = FileStream(program_path)
     lexer = jjLexer(data)
     stream = CommonTokenStream(lexer)
     parser = jjParser(stream)
+    parser.removeErrorListeners()
+    error_listener = jjErrorListener()
+    parser.addErrorListener(error_listener)
+    if error_listener.has_syntax_error:
+        return True
+
     listener = jjListener(verbose)
 
     tree = parser.prog()
@@ -21,10 +30,12 @@ def interpret(program_path: str):
     stream = CommonTokenStream(lexer)
     parser = jjParser(stream)
     tree = parser.prog()
-    
+
     visitor = jjVisitor()
     visitor.visit(tree)
 
+
 def run(program_path: str):
-    check_initial_errors(program_path, verbose=False)
+    if check_initial_errors(program_path, verbose=False):
+        return
     interpret(program_path)
